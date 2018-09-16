9147ccb399ccc2dbde7c6041ff507d74f92ff0d0
H. Chaminaud <herve@chaminaud.net>
2018-09-16 11:55:30 +0200

    first commit

---
+++ ex2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_first_arg(int argc, char *argv[])
+{
+    if (argc <2)
+    {
+        printf("no argument\n\n");
+        exit(0);
+    }
+    return (argv[1]);
+}
+
+int main(int argc, char *argv[])
+{
+    char *arg = get_first_arg(argc, argv);
+
+    printf("chaine: %s", arg);
+
+    return 0;
+}
+
